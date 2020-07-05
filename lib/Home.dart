4d717450811875227c82a014c3c08f232afc1557
Adeilson Silva <adeilson@gmail.com>
2020-07-04 21:36:06 -0300

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  void _abrir(String tipo) {
+    print('tipo: $tipo');
+
+    switch (tipo) {
+      case 'empresa':
+        break;
+      default:
+        new Exception('Tipo não definido');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('ATM Consultoria'),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/logo.png'),
+            Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () => _abrir('empresa'),
+                    child: Image.asset('assets/images/menu_empresa.png'),
+                  ),
+                  GestureDetector(
+                    onTap: () => _abrir('servico'),
+                    child: Image.asset('assets/images/menu_servico.png'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () => _abrir('cliente'),
+                    child: Image.asset('assets/images/menu_cliente.png'),
+                  ),
+                  GestureDetector(
+                    onTap: () => _abrir('contato'),
+                    child: Image.asset('assets/images/menu_contato.png'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
